e42009b0f06789fd2f9fabb42b89c997a50f58b5
abdallah <handasa@MacBook-Air-abdallah.local>
2026-09-17 15:11:19 +0300

    Add Taala Afhemak reel thumbnails

--- app/page.tsx
+++ app/page.tsx
@@ -101,11 +101,11 @@ const ebtkaratDarkAds = [
 const ebtkaratDarkAdsUrl="https://drive.google.com/drive/folders/16vXu45qimcXV2mat3NCdcWUbn5go0Wwk";
 
 const talaaReels = [
-  {id:"DUqsnqXjM3s", label:"The starting point", ar:"نقطة البداية", kind:"text"},
-  {id:"DU8gZMvDCrP", label:"Ramadan system", ar:"سلسلة رمضان", kind:"text"},
+  {id:"DUqsnqXjM3s", label:"The starting point", ar:"نقطة البداية", image:"https://www.instagram.com/p/DUqsnqXjM3s/media/?size=l"},
+  {id:"DU8gZMvDCrP", label:"Ramadan system", ar:"سلسلة رمضان", image:"https://www.instagram.com/p/DU8gZMvDCrP/media/?size=l"},
   {id:"DWR6Mb7sDgU", label:"The third direction begins", ar:"بداية الديركشن الثالث", image:"/proof/DWR6Mb7sDgU.jpg"},
-  {id:"DZf22pwMcfX", label:"The new language works", ar:"اللغة الجديدة بدأت تشتغل", kind:"text"},
-  {id:"Dcg61JzInAV", label:"A repeatable format", ar:"فورمات قابل للتكرار", kind:"text"},
+  {id:"DZf22pwMcfX", label:"The new language works", ar:"اللغة الجديدة بدأت تشتغل", image:"https://www.instagram.com/p/DZf22pwMcfX/media/?size=l"},
+  {id:"Dcg61JzInAV", label:"A repeatable format", ar:"فورمات قابل للتكرار", image:"https://www.instagram.com/p/Dcg61JzInAV/media/?size=l"},
 ];
 
 export default function Home(){
